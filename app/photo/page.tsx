6838835fa81ca4ab6f9c18f6ef71a9a653060717
Jiwon Kwon <kjiwon411@gmail.com>
2024-05-20 03:15:27 +0900

    feat: position the string with spinning effect

--- app/photo/page.tsx
+++ app/photo/page.tsx
@@ -8,25 +8,37 @@ export default function Photo() {
             const response = await fetch('/api');
             const data = await response.json();
             console.log('API response:', data);
-            if (data.success) {
-                setPhotoData(data.base64Photo);
-            } else {
-                console.log(data.message);
-            }
-            console.log(photoData);
-
+            
+            const base64Data = data.base64Data;
+            setPhotoData(base64Data);
+            console.log('Photo data:', base64Data);
+            
         } catch (e) {
             console.error(e);
         }
     }
 
     useEffect(() => {
-        console.log(photoData);
         getData();
-    }, [photoData]);
+    });
 
-    
     return (
-        <p className="text-black">{photoData}</p>
-    );
+        <div className='bg-[#FF80FB] w-screen h-screen flex flex-col items-center justify-center overflow-y-auto'>
+          <p className="text-[#0CBA00] text-[200px] w-full text-center whitespace-pre-wrap overflow-wrap break-word tracking-wide transform rotate-45">  
+              {photoData}
+          </p>
+          <p className="text-[#0CBA00] text-[200px] w-full text-center whitespace-pre-wrap overflow-wrap break-word tracking-widest transform -rotate-45">  
+              {photoData}
+          </p>
+          <p className="text-[#0CBA00] text-[200px] w-full text-center whitespace-pre-wrap overflow-wrap break-word tracking-wider transform skew-x-12">  
+              {photoData}
+          </p>
+          <p className="text-[#0CBA00] text-[200px] w-full text-center whitespace-pre-wrap overflow-wrap break-word tracking-wide transform scale-150">  
+              {photoData}
+          </p>
+          <p className="text-[#0CBA00] text-[200px] w-full text-center whitespace-pre-wrap overflow-wrap break-word tracking-widest transform scale-50">  
+              {photoData}
+          </p>
+        </div>
+      );
 }
