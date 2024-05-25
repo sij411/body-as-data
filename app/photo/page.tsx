71ab2454391a4f7a678836943b342c2cdc95fd46
Jiwon Kwon <kjiwon411@gmail.com>
2024-05-25 11:52:17 +0900

    Add return to main button

--- app/photo/page.tsx
+++ app/photo/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { useEffect, useState } from 'react';
 import styles from './Photo.module.css';
+import Link from 'next/link';
 
 export default function Photo() {
     const [photoData, setPhotoData] = useState(null);
@@ -33,9 +34,14 @@ export default function Photo() {
     return (
         <div className='bg-[#FF80FB] w-screen h-screen  overflow-hidden'>
             <div id="photo-data-container" className={styles.photoDataContainer}>  
-                {photoData !== null ? (photoData as string).repeat(30) : 'EMPTY'}
+                {photoData !== null ? (photoData as string).repeat(30) : ''}
             </div>
-            {/* TODO : add something cool as a fallback value */}
+
+            <div id='return-to-main-button' className='fixed bottom-16 right-20'>
+                <Link href='/webcam'>
+                <button className='rounded-s bg-[#0CBA00] px-1 text-center'>RETURN</button>
+                </Link>
+            </div>
         </div>
     );
 }
